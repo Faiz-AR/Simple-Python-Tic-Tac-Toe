ce46abb21fbb68518027502e33b4ea18ab35ba3f
Faiz-AR <74257945+Faiz-AR@users.noreply.github.com>
2020-12-02 20:17:48 +0800

    Modified code using Pylint styling

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,21 @@
+'''
+A SIMPLE TIC-TAC-TOE GAME
+'''
+import sys
 #   FUNCTIONS   #
 def game_board(board):
+    '''
+    To display/update gameboard after each turn
+    '''
     print('\n')
     print(str(board[0:3])+"\n"+str(board[3:6])+"\n"+str(board[6:9]))
     print('---------------')
     print("[ 1 , 2 , 3 ]\n[ 4 , 5 , 6 ]\n[ 7 , 8 , 9 ]")
 
 def player_choice():
+    '''
+    To return the player's choice and notify if the choice is out of range or isn't a number
+    '''
     number_range = list(range(1,10))
     choice = ''
     while choice not in number_range:
@@ -13,28 +23,31 @@ def player_choice():
             choice = int(input("Enter a number between 1-9: "))
             if choice not in number_range:
                 print("Sorry, the number you entered is out of range! Please try again")
-        except:
+        except TypeError:
             print("Please enter a number value!")
-    
     return int(choice)-1   
 
 def player_input():
+    '''
+    To record player 1 and player 2 marker
+    '''
     marker = ''
 
-    while marker != 'X' and marker != 'O':
+    while marker not in ['X','O']:
         marker = input('Player 1, choose X or O: ')
-        if marker != 'X' and marker != 'O':
+        if marker not in ['X','O']:
             print("Enter X or O!")
-
     player1 = marker
     if player1 == 'X':
         player2 = 'O'
     else:
         player2 = 'X'
-    
     return (player1, player2)
 
 def replace(board,choice,mark):
+    '''
+    Replace the '-' in the board with the player's marker and position choice
+    '''
     valid = False
 
     if board[choice] != '-':
@@ -42,10 +55,13 @@ def replace(board,choice,mark):
     else:
         board[choice] = mark
         valid = True
-    
+
     return valid
 
 def check(mark):
+    '''
+    To check for win/draw conditions after each turn
+    '''
     #HORIZONTAL WIN
     if board[0] == mark and board[1] == mark and board[2] == mark or  board[3] == mark and board[4] == mark and board[5] == mark or  board[6] == mark and board[7] == mark and board[8] == mark:
         print("Congrats! Player "+ mark + " wins the game!")
@@ -69,37 +85,36 @@ def check(mark):
         return False
 
 #   GAME   #
-global counter
-global m
-game = True
-m = ''
-counter = 1
+global COUNTER
+global MARK
+MARK = ''
+COUNTER = 1
+GAME = True
 board = ['-','-','-', '-','-','-','-','-','-']
-         
 print("Welcome to Tic-Tac-Toe!")
 while True:
     player1_marker, player2_marker = player_input()
-    while game == True:
+    while GAME is True:
         game_board(board)
-        if counter % 2 != 0:
-            m = player1_marker
-            print('Player 1('+m+') turn!')
+        if COUNTER % 2 != 0:
+            MARK = player1_marker
+            print('Player 1('+MARK+') turn!')
         else:
-            m = player2_marker
-            print('Player 2('+m+') turn!')
-        choice = player_choice()
-        if replace(board,choice,m) ==  False:
+            MARK = player2_marker
+            print('Player 2('+MARK+') turn!')
+        CHOICE = player_choice()
+        if replace(board,CHOICE,MARK) is  False:
             print('Space taken! Try again')
             continue
-        if check(m) == True:
-            game = False
+        if check(MARK) is True:
+            GAME = False
             replay = input('Play Again? (Y/N): ')
             if replay == 'Y':
-                game = True
+                GAME = True
                 board = ['-','-','-','-','-','-','-','-','-']
                 game_board(board)
                 break
             else:
-                exit()
+                sys.exit()
         else:
-            counter +=1     
+            COUNTER +=1
